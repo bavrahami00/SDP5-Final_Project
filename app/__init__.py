a8f67bd25ec615f0fc703fd3a06c5604e9dbfdcb
bavrahami00 <bavrahami00@stuy.edu>
2020-06-07 20:54:58 -0400

    Carried over login process from previous project

---
+++ app/__init__.py
@@ -0,0 +1,81 @@
+import os
+from flask import Flask, render_template, session, url_for, redirect
+
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
+
+
+#=====DECORATOR=FUNCTIONS===================================================
+# Decorator functions to eliminate redundancy:
+
+# Login checking
+def protected(route_function):
+    def login_check(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for("login"))
+        return route_function(*args, **kwargs)
+    login_check.__name__ = route_function.__name__
+    return login_check
+
+#============================================================================
+
+
+
+
+
+# root redirects to login if the user isn't logged in, and home if they are
+@app.route("/")
+def root():
+    # if user is logged in, redirect to home
+    if "username" in session:
+        return redirect(url_for("home"))
+    # else redirect to login
+    return redirect(url_for("login"))
+
+
+@app.route("/login")
+def login():
+    # if already logged in, redirect to home
+    if 'username' in session:
+        return redirect(url_for("home"))
+    # make sure the login attempt has both a username and a password
+    if len(request.args) == 2:
+        # database checks the username and password
+        if db.authenticate_user(request.args["username"], request.args["password"]):
+            # Username is added to the session information
+            session["username"] = request.args["username"]
+            # The user is now logged in and goes to the home page
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid username/password combination")
+    else:
+        flash("Enter both a username and a password")
+    return render_template("login.html")
+
+
+# register page and validation
+@app.route("/register")
+def register():
+    # if user is logged in, redirect to home
+    if "username" in session:
+        return redirect(url_for("home"))
+    # if user attempts registration
+    if len(request.args) >= 3:
+        # if any one of the three fields are blank, flash error
+        if request.args["username"] == "" or request.args["password1"] == "" or request.args["password2"] == "":
+            flash("Please do not leave any fields blank.")
+        # if the passwords don't match, flash error
+        elif request.args["password1"] != request.args["password2"]:
+            flash("Passwords don't match.")
+        # else if adding the user (to the database) is successful, username must be unique
+        elif db.add_user(request.args["username"], request.args["password1"]):
+            # if the username is unique, session is added and user is redirected to home
+            session["username"] = request.args["username"]
+            return redirect(url_for("home"))
+        # else flash error
+        else:
+            flash("Username not unique.")
+    # render register template
+    return render_template('register.html')
